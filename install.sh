315d5cbb8935c202abe575b1e1db699ee136e8c7
bmchina <85761490+bmchina@users.noreply.github.com>
2021-06-23 23:46:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 sudo cp 98-usb4g.rules /etc/udev/rules.d
 sudo cp usbin.sh /usr/local/bin
 sudo cp start4g.py /usr/local/bin
+echo -e "Installation completed, please REBOOT your device to take effaction."
